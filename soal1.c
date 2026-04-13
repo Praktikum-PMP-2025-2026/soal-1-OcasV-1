68f378d90e420357747843a2dbf06e80e0b07d30
OcasV <farrasfuadyh@gmail.com>
2026-04-13 08:41:27 +0700

    #001
    Implement status validation based on permission level, body temperature, radiation level, and arrival time.

--- soal1.c
+++ soal1.c
@@ -1,5 +1,51 @@
+/* EL2008 Praktikum Pemecahan Masalah dengan Pemrograman
+* Modul : 1 - Overview Of C Language
+* Pembuat : Farras Fuady Hakim
+* Deskripsi : Program untuk menentukan status akhir peneliti berdasarkan level izin, suhu tubuh, kadar radiasi, dan jam kedatnagan.
+*/
 #include <stdio.h>
 
-int main(void) {
+// validasi : 0(Masuk), 1(Karantina), 2(Tolak), 3(Pemerikasaan)
+int cekStatus(int izin, int suhu, int radiasi, int jam) {
+    int validasi;
+    if (radiasi >= 6) {
+        validasi = 2;
+    } else if (suhu >= 390) {
+        validasi = 1;
+    } else if (izin == 1 && (jam < 6 || jam > 20)) {
+        validasi = 2;
+    } else if (izin == 1) {
+        validasi = 0;
+    } else if (izin == 2 && radiasi <= 2 && 8 <= jam <= 18) {
+        validasi = 0;
+    } else if (izin == 2) {
+        validasi = 3;
+    } else if (izin == 3 && radiasi == 0 && suhu < 380) {
+        validasi = 0;
+    } else {
+        validasi = 2;
+    }
+
+    return validasi;
+}
+
+int main() {
+    int izin, suhu, radiasi, jam, validasi;
+
+    printf("Masukkan input (izin, suhu, radiasi, jam) : ");
+    scanf("%d %d %d %d", &izin, &suhu, &radiasi, &jam);
+
+    switch (validasi) {
+    case 0:
+        printf("MASUK");
+    case 1:
+        printf("KARANTINA");
+    case 2:
+        printf("TOLAK");
+    case 3:
+        printf("PEMERIKSAAN");
+    default:
+        break;
+    }
     return 0;
 }
